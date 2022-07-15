aec9281bf51d276abf64cc8692a2f5ed2a4eea74
yasirhussainccl <101343077+yasirhussainccl@users.noreply.github.com>
2022-07-15 12:28:20 +0500

    fixed typo

--- local/jenkins-job-script.ps1
+++ local/jenkins-job-script.ps1
@@ -1,5 +1,5 @@
 ﻿#$PSDefaultParameterValues = @{ '*:Force' = $true; '*:Confirm' = $false }
-Import-Module -Name AutomatedLab -Force -confirm:$false
+Import-Module -Name AutomatedLab -Force -Confirm:$false
 try {
     Import-Lab -Name $env:COMPUTERNAME
 }
