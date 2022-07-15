b57c4f99f25f0e6d5f9b36961a2ef8f48a8afca2
yasirhussainccl <101343077+yasirhussainccl@users.noreply.github.com>
2022-07-15 17:03:10 +0500

    undo

--- local/jenkins-job-script.ps1
+++ local/jenkins-job-script.ps1
@@ -13,7 +13,7 @@ catch {
     #param ([Parameter(Mandatory)]$LabName, [Parameter(Mandatory)]$VMName)
     $LabName = $env:COMPUTERNAME
     #$VMName = ${env:JOB_NAME}
-    $VMName = 'cvweb-latestbuild'
+    $VMName = $env:COMPUTERNAME
 
     # default network switch for internet conectivity
     $defaultNetworkSwitch = 'Default Switch'
@@ -28,19 +28,19 @@ catch {
     Add-LabIsoImageDefinition -Name SQLServer2019 -Path $labSources\ISOs\SQLServer2019-x64-ENU.iso
 
     # define VM
-    Add-LabMachineDefinition -Name 'cvweb-latestbuild' -Memory 2GB -Network $defaultNetworkSwitch -Roles SQLServer2019 -OperatingSystem 'Windows 10 Enterprise Evaluation' 
+    Add-LabMachineDefinition -Name $VMName -Memory 2GB -Network $defaultNetworkSwitch -Roles SQLServer2019 -OperatingSystem 'Windows 10 Enterprise Evaluation' 
 
     Install-Lab -Verbose
 
     # grant service logon privilege to the default user
-    Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName 'cvweb-latestbuild' -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
+    Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName $VMName -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
     
     #install jdk-8 - x64
-    Install-LabSoftwarePackage -ComputerName 'cvweb-latestbuild' -Path $labSources\SoftwarePackages\jdk-8u321-windows-x64.exe -CommandLine '/s /log C:\DeployDebug\Java.log /Q' -Verbose
+    Install-LabSoftwarePackage -ComputerName $VMName -Path $labSources\SoftwarePackages\jdk-8u321-windows-x64.exe -CommandLine '/s /log C:\DeployDebug\Java.log /Q' -Verbose
 
     Show-LabDeploymentSummary -Detailed
 
-    Checkpoint-LabVM -ComputerName 'cvweb-latestbuild' -SnapshotName 'After creating a new VM with win10, jdk8 and SQLServer2019'
+    Checkpoint-LabVM -ComputerName $VMName -SnapshotName 'After creating a new VM with win10, jdk8 and SQLServer2019'
 
     $CVLocalPath = "D:\Jenkins\latestbuild\cvWeb\Deployment\build\setup\cvweb.msi"
 
