8e1ae1b8332fc3d3718d720672181f6ae70cda73
yasirhussainccl <101343077+yasirhussainccl@users.noreply.github.com>
2022-08-02 18:34:57 +0500

    update jenkins-install-cvweb script

--- local/jenkins-install-cvweb.ps1
+++ local/jenkins-install-cvweb.ps1
@@ -1,6 +1,226 @@
 param ([Parameter(Mandatory)]$VMName, [Parameter(Mandatory)]$CVwebVersion)
 
-$ccissPath = "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVWebVersion\Install\cvweb.msi"
+$VMName = "$VMName $CVwebVersion"
+
+Import-Module -Name AutomatedLab -Force
+
+$LabName = $env:COMPUTERNAME.Replace('-', '')
+
+# default network switch for internet conectivity
+$defaultNetworkSwitch = 'Default Switch'
+
+# create an empty lab template and define where the lab XML files and the VMs will be stored
+New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV -VmPath C:\AutomatedLab-VMs
+
+#make the network definition
+Add-LabVirtualNetworkDefinition -Name $defaultNetworkSwitch -HyperVProperties @{ SwitchType = 'Internal'; AdapterName = 'vEthernet' }
+
+# read all ISOs in the LabSources folder and add the SQL 2019 ISO
+#Add-LabIsoImageDefinition -Name SQLServer2019 -Path $labSources\ISOs\SQLServer2019-x64-ENU.iso
+
+# define VM
+Add-LabMachineDefinition -Name "$VMName" -Memory 6GB -Network $defaultNetworkSwitch -Roles SQLServer2019 -OperatingSystem 'Windows Server 2019 Standard (Desktop Experience)'
+
+Install-Lab -Verbose
+
+# grant service logon privilege to the default user
+Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName "$VMName" -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
+    
+#install jdk-8 - x64
+Install-LabSoftwarePackage -ComputerName "$VMName" -Path $labSources\SoftwarePackages\jdk-8u321-windows-x64.exe -CommandLine '/s /log C:\DeployDebug\Java.log /Q' -Verbose
+
+# install open jdk 8
+#Install-LabSoftwarePackage -ComputerName $VMName -Path 'C:\LabSources\SoftwarePackages\OpenJDK8U-jdk_x64_windows_hotspot_8u342b07.msi' -CommandLine '/qn /log C:\DeployDebug\OpenJDK.log' -Verbose
+
+Show-LabDeploymentSummary -Detailed
+
+# create checkpoint
+Checkpoint-LabVM -ComputerName $VMName -SnapshotName 'After the installation of WinServer2019, jdk8 and SQLServer2019'
+
+$CVTransforms = "TRANSFORMS=:cvweb_x64.mst;"
+
+if($CVWebVersion -eq 'latestbuild') {
+    $sourceFolderPath = "\\cciss-build\latestbuild\cvWeb\Deployment\build\setup"
+    $CVLocalPath = "C:\cvweb\setup\cvweb.msi"
+    $destinationFolderPath = "C:\cvweb\"
+}
+else {
+    $sourceFolderPath = "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install"
+    $CVLocalPath = "C:\cvwebSetup\$CVwebVersion\Install\cvweb.msi"
+    $destinationFolderPath = "C:\cvweb\$CVwebVersion"
+
+    if(Test-Path -Path "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install\cvweb_x64.mst") {
+        $CVTransforms = "TRANSFORMS=cvweb_x64.mst;"    
+    }
+}
+
+Copy-LabFileItem -Path $sourceFolderPath -ComputerName $VMName -DestinationFolderPath $destinationFolderPath -Recurse -Verbose 
+
+$logFileName = "`"C:\DeployDebug\cvweb $CVwebVersion $(Get-Date -Format "yyyy-MM-dd").log`""
+
+$CVParams = "/qn /log $logFileName DBSERVERNAME=`"$VMName`" MSSQLSERVERNAME=`"$VMName`" CVDOMAIN=Production INSTALLDIR=`"C:\Program Files (x86)\Clinical Computing\cvwebappserver\`" SQLINSTANCE_JTDS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"localhost;integratedSecurity=true`" CREATENEWCVDB.4B175C70_94AB_42E4_B485_1478B3DF7933=1 LOCALEARGS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"-Dcv.locale=ClinicalVisionCore:SystemSettings.UnitedKingdom -Dcv.language=ClinicalVisionCore:SystemSettings.UKEnglish`" CVLANGUAGE=GB UPGRADEOPTION=new MYUSERNAME=`"$VMName\Administrator`" MYPASSWORD=Somepass1 NTDOMAIN=$VMName NTUSER=Administrator $CVTransforms PROCARCHITECTURE=`"x64`" INSTANCEID=default SSLPORT=443 CVINTPORT=8448 JVMMS=1024 JVMMX=2048"
+
+# install cvweb    
+Install-LabSoftwarePackage -ComputerName $VMName -LocalPath $CVLocalPath -CommandLine $CVParams -Verbose -Timeout 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try {
+    # try to import lab
+    Import-Lab -Name $LabName
+    
+    # get lab name
+    $lab = Get-Lab
+    
+    # get vm name
+    $vm = Get-labVM | Select-Object Name | Where-Object {$_.Name -eq '$CVwebVersion'}
+}
+catch {
+    Write-Host "$LabName Lab not found!"
+}
+
+if($lab.Name -eq $LabName -and $vm.Name -eq $VMName) {
+    Write-Host "$VMName found and hosted at $LabName"
+
+    # remove old copy of cvweb setup from vm
+    $sb = {
+        if(Test-Path -Path "C:\cvweb\setup") {
+            Remove-Item 'C:\cvweb\setup' -Recurse -Force
+        }
+    }
+    Invoke-LabCommand -ActivityName 'Remove Old cvweb verison' -ComputerName $VMName -ScriptBlock $sb  -UseLocalCredential
+
+    # copy latestbuild of cvweb setup from cciss-build
+    Copy-LabFileItem -Path $sourceFolderPath -ComputerName $VMName -DestinationFolderPath $destinationFolderPath -Recurse -Verbose
+
+}
+else {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+$VMName = "$CVwebVersion"
+$VMTempDirectory = 'C:\temp'
+$VMCVWebDirectory = 'C:\cvweb\setup'
+$sourceFolderPath = "\\cciss-build\latestbuild\cvWeb\Deployment\build\setup"
+$destinationFolderPath = "C:\cvweb\"
+$CVLocalPath = "C:\cvweb\setup\cvweb.msi"
+
+if(Test-Path -Path "\\cciss-build\latestbuild\cvWeb\Deployment\build\setup\cvweb_x64.mst") {
+    $CVTransforms = "TRANSFORMS=cvweb_x64.mst;"    
+}
+else {
+    $CVTransforms = "TRANSFORMS=:cvweb_x64.mst;"
+}
+
+try {
+    # try to import lab
+    Import-Lab -Name $LabName
+    
+    # get lab name
+    $lab = Get-Lab
+    
+    # get vm name
+    $vm = Get-LabVM
+}
+catch {
+    Write-Host "$LabName Lab not found!"
+}
+
+if($lab.Name -eq $LabName -and $vm.Name -eq $VMName) {
+    Write-Host "$VMName found and hosted at $LabName"
+
+    # remove old copy of cvweb setup from vm
+    $sb = {
+        if(Test-Path -Path "C:\cvweb\setup") {
+            Remove-Item 'C:\cvweb\setup' -Recurse -Force
+        }
+    }
+    Invoke-LabCommand -ActivityName 'Remove Old cvweb verison' -ComputerName $VMName -ScriptBlock $sb  -UseLocalCredential
+
+    # copy latestbuild of cvweb setup from cciss-build
+    Copy-LabFileItem -Path $sourceFolderPath -ComputerName $VMName -DestinationFolderPath $destinationFolderPath -Recurse -Verbose
+
+}
+else {
+    # default network switch for internet conectivity
+    $defaultNetworkSwitch = 'Default Switch'
+
+    # create an empty lab template and define where the lab XML files and the VMs will be stored
+    New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV -VmPath C:\AutomatedLab-VMs
+
+    #make the network definition
+    Add-LabVirtualNetworkDefinition -Name $defaultNetworkSwitch -HyperVProperties @{ SwitchType = 'Internal'; AdapterName = 'vEthernet' }
+
+    # read all ISOs in the LabSources folder and add the SQL 2019 ISO
+    Add-LabIsoImageDefinition -Name SQLServer2019 -Path $labSources\ISOs\SQLServer2019-x64-ENU.iso
+
+    # define VM
+    Add-LabMachineDefinition -Name $VMName -Memory 6GB -Network $defaultNetworkSwitch -Roles SQLServer2019 -OperatingSystem 'Windows Server 2019 Standard (Desktop Experience)'
+
+    Install-Lab -Verbose
+
+    # grant service logon privilege to the default user
+    Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName $VMName -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
+    
+    #install jdk-8 - x64
+    Install-LabSoftwarePackage -ComputerName $VMName -Path $labSources\SoftwarePackages\jdk-8u321-windows-x64.exe -CommandLine '/s /log C:\DeployDebug\Java.log /Q' -Verbose
+
+    # install open jdk 8
+    #Install-LabSoftwarePackage -ComputerName $VMName -Path 'C:\LabSources\SoftwarePackages\OpenJDK8U-jdk_x64_windows_hotspot_8u342b07.msi' -CommandLine '/qn /log C:\DeployDebug\OpenJDK.log' -Verbose
+
+    Show-LabDeploymentSummary -Detailed
+
+    # create checkpoint
+    Checkpoint-LabVM -ComputerName $VMName -SnapshotName 'After the installation of WinServer2019, jdk8 and SQLServer2019'
+}
+
+
+
+
+
+
 
 Write-Output $ccissPath
 
