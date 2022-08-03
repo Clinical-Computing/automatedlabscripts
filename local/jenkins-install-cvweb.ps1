1e74093e2f1eb1d7c34e802e8d5f6e01483f5ffb
yasirhussainccl <101343077+yasirhussainccl@users.noreply.github.com>
2022-08-03 14:32:22 +0500

    comment cvweb part.

--- local/jenkins-install-cvweb.ps1
+++ local/jenkins-install-cvweb.ps1
@@ -42,28 +42,28 @@ Show-LabDeploymentSummary -Detailed
 # create checkpoint
 Checkpoint-LabVM -ComputerName $VMName -SnapshotName 'After the installation of WinServer2019, jdk8 and SQLServer2019'
 
-$CVTransforms = "TRANSFORMS=:cvweb_x64.mst;"
+#$CVTransforms = "TRANSFORMS=:cvweb_x64.mst;"
 
-if($CVWebVersion -eq 'latestbuild') {
-    $sourceFolderPath = "\\cciss-build\latestbuild\cvWeb\Deployment\build\setup"
-    $CVLocalPath = "C:\cvweb\setup\cvweb.msi"
-    $destinationFolderPath = "C:\cvweb\"
-}
-else {
-    $sourceFolderPath = "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install"
-    $CVLocalPath = "C:\cvweb\$CVwebVersion\Install\cvweb.msi"
-    $destinationFolderPath = "C:\cvweb\$CVwebVersion"
+#if($CVWebVersion -eq 'latestbuild') {
+#    $sourceFolderPath = "\\cciss-build\latestbuild\cvWeb\Deployment\build\setup"
+#    $CVLocalPath = "C:\cvweb\setup\cvweb.msi"
+#    $destinationFolderPath = "C:\cvweb\"
+#}
+#else {
+#    $sourceFolderPath = "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install"
+#    $CVLocalPath = "C:\cvweb\$CVwebVersion\Install\cvweb.msi"
+#    $destinationFolderPath = "C:\cvweb\$CVwebVersion"
 
-    if(Test-Path -Path "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install\cvweb_x64.mst") {
-        $CVTransforms = "TRANSFORMS=cvweb_x64.mst;"    
-    }
-}
+#    if(Test-Path -Path "\\cciss-file\Product\clinicalvision5\Install\General Availability\$CVwebVersion\Install\cvweb_x64.mst") {
+#        $CVTransforms = "TRANSFORMS=cvweb_x64.mst;"    
+#    }
+#}
 
-Copy-LabFileItem -Path $sourceFolderPath -ComputerName $VMName -DestinationFolderPath $destinationFolderPath -Recurse -Verbose 
+#Copy-LabFileItem -Path $sourceFolderPath -ComputerName $VMName -DestinationFolderPath $destinationFolderPath -Recurse -Verbose 
 
-$logFileName = "`"C:\DeployDebug\cvweb $CVwebVersion $(Get-Date -Format "yyyy-MM-dd").log`""
+#$logFileName = "`"C:\DeployDebug\cvweb $CVwebVersion $(Get-Date -Format "yyyy-MM-dd").log`""
 
-$CVParams = "/qn /log $logFileName DBSERVERNAME=`"$VMName`" MSSQLSERVERNAME=`"$VMName`" CVDOMAIN=Production INSTALLDIR=`"C:\Program Files (x86)\Clinical Computing\cvwebappserver\`" SQLINSTANCE_JTDS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"localhost;integratedSecurity=true`" CREATENEWCVDB.4B175C70_94AB_42E4_B485_1478B3DF7933=1 LOCALEARGS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"-Dcv.locale=ClinicalVisionCore:SystemSettings.UnitedKingdom -Dcv.language=ClinicalVisionCore:SystemSettings.UKEnglish`" CVLANGUAGE=GB UPGRADEOPTION=new MYUSERNAME=`"$VMName\Administrator`" MYPASSWORD=Somepass1 NTDOMAIN=$VMName NTUSER=Administrator $CVTransforms PROCARCHITECTURE=`"x64`" INSTANCEID=default SSLPORT=443 CVINTPORT=8448 JVMMS=1024 JVMMX=2048"
+#$CVParams = "/qn /log $logFileName DBSERVERNAME=`"$VMName`" MSSQLSERVERNAME=`"$VMName`" CVDOMAIN=Production INSTALLDIR=`"C:\Program Files (x86)\Clinical Computing\cvwebappserver\`" SQLINSTANCE_JTDS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"localhost;integratedSecurity=true`" CREATENEWCVDB.4B175C70_94AB_42E4_B485_1478B3DF7933=1 LOCALEARGS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"-Dcv.locale=ClinicalVisionCore:SystemSettings.UnitedKingdom -Dcv.language=ClinicalVisionCore:SystemSettings.UKEnglish`" CVLANGUAGE=GB UPGRADEOPTION=new MYUSERNAME=`"$VMName\Administrator`" MYPASSWORD=Somepass1 NTDOMAIN=$VMName NTUSER=Administrator $CVTransforms PROCARCHITECTURE=`"x64`" INSTANCEID=default SSLPORT=443 CVINTPORT=8448 JVMMS=1024 JVMMX=2048"
 
 # install cvweb    
 #Install-LabSoftwarePackage -ComputerName $VMName -LocalPath $CVLocalPath -CommandLine $CVParams -Verbose -Timeout 30
