39cdd741fa31849d0f8ddfb11dd702c89dbbd504
yasirhussainccl <101343077+yasirhussainccl@users.noreply.github.com>
2022-08-03 14:13:43 +0500

    remove sql server role and cvweb installation

--- local/jenkins-install-cvweb.ps1
+++ local/jenkins-install-cvweb.ps1
@@ -18,17 +18,17 @@ New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV -VmPath C:\
 Add-LabVirtualNetworkDefinition -Name $defaultNetworkSwitch -HyperVProperties @{ SwitchType = 'Internal'; AdapterName = 'vEthernet' }
 
 # read all ISOs in the LabSources folder and add the SQL 2019 ISO
-Add-LabIsoImageDefinition -Name SQLServer2019 -Path $labSources\ISOs\SW_DVD9_NTRL_SQL_Svr_Standard_Edtn_2019Dec2019_64Bit_English_OEM_VL_X22-22109.iso
+#Add-LabIsoImageDefinition -Name SQLServer2019 -Path $labSources\ISOs\SW_DVD9_NTRL_SQL_Svr_Standard_Edtn_2019Dec2019_64Bit_English_OEM_VL_X22-22109.iso
 
-$role = Get-LabMachineRoleDefinition -Role SQLServer2019 -Properties @{Features = 'SQL,Tools'}
+#$role = Get-LabMachineRoleDefinition -Role SQLServer2019 -Properties @{Features = 'SQL,Tools'}
 
 # define VM
-Add-LabMachineDefinition -Name "$VMName" -Memory 6GB -Network $defaultNetworkSwitch -Roles $role -OperatingSystem 'Windows Server 2019 Standard (Desktop Experience)'
+Add-LabMachineDefinition -Name "$VMName" -Memory 6GB -Network $defaultNetworkSwitch -OperatingSystem 'Windows Server 2019 Standard (Desktop Experience)'
 
 Install-Lab -Verbose
 
 # grant service logon privilege to the default user
-Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName "$VMName" -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
+#Invoke-LabCommand -ActivityName 'Grant Service-Logon Privilege to the default user' -ComputerName "$VMName" -FilePath $labSources\CustomScripts\grant_service_logon_privilege.ps1 -Verbose
 #Enable-LabAutoLogon -ComputerName "$VMName"
     
 #install jdk-8 - x64
@@ -66,4 +66,4 @@ $logFileName = "`"C:\DeployDebug\cvweb $CVwebVersion $(Get-Date -Format "yyyy-MM
 $CVParams = "/qn /log $logFileName DBSERVERNAME=`"$VMName`" MSSQLSERVERNAME=`"$VMName`" CVDOMAIN=Production INSTALLDIR=`"C:\Program Files (x86)\Clinical Computing\cvwebappserver\`" SQLINSTANCE_JTDS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"localhost;integratedSecurity=true`" CREATENEWCVDB.4B175C70_94AB_42E4_B485_1478B3DF7933=1 LOCALEARGS.4B175C70_94AB_42E4_B485_1478B3DF7933=`"-Dcv.locale=ClinicalVisionCore:SystemSettings.UnitedKingdom -Dcv.language=ClinicalVisionCore:SystemSettings.UKEnglish`" CVLANGUAGE=GB UPGRADEOPTION=new MYUSERNAME=`"$VMName\Administrator`" MYPASSWORD=Somepass1 NTDOMAIN=$VMName NTUSER=Administrator $CVTransforms PROCARCHITECTURE=`"x64`" INSTANCEID=default SSLPORT=443 CVINTPORT=8448 JVMMS=1024 JVMMX=2048"
 
 # install cvweb    
-Install-LabSoftwarePackage -ComputerName $VMName -LocalPath $CVLocalPath -CommandLine $CVParams -Verbose -Timeout 30
+#Install-LabSoftwarePackage -ComputerName $VMName -LocalPath $CVLocalPath -CommandLine $CVParams -Verbose -Timeout 30
